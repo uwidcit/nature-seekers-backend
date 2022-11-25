481203fb5cf799d4e8013e49293ff3a09e1bd536
afeef10 <118063717+afeef10@users.noreply.github.com>
2022-11-24 23:33:55 -0400

    Create turtletag.py

---
+++ App/controllers/turtletag.py
@@ -0,0 +1,33 @@
+from App.models import TurtleTag
+from App.database import db
+
+def create_turtle_tag (tageventid, code, status, location):
+    new_turtletag = TurtleTag (tageventid=tageventid, code=code, status=status, location=location)
+
+    db.session.add(new_turtletag)
+    db.session.commit()
+
+    return new_turtletag
+
+
+def get_turtle_tag (id):
+    return TurtleTag.query.get(id)
+
+
+def update_turtleTag (id, status=None, location=None):
+    turtle_tag = get_turtle_tag(id)
+
+    if turtle_tag:
+
+        if status:
+            TurtleTag.status = status
+
+        if location:
+            TurtleTag.location = location
+
+        db.session.add(turtle_tag)
+        db.session.commit()
+
+        return turtle_tag
+
+    return None
