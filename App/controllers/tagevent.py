22055bce96698d4b15a2ae767d87d4c82a37b05e
afeef10 <118063717+afeef10@users.noreply.github.com>
2022-11-25 09:16:09 -0400

    Update tagevent.py

--- App/controllers/tagevent.py
+++ App/controllers/tagevent.py
@@ -1,10 +1,26 @@
-from App.models import TagEvent
+from App.models import Turtle, User, TagEvent
 from App.database import db
 
+
+def get_turtle (id):
+    return Turtle.query.get(id)
+
+
+def get_user (id):
+    return User.query.get(id)
+
+
 def create_tag_event (turtleid, userid, comments, timestamp, weight, length, lat, lon):
-    new_tagEvent = TagEvent (turtleid=turtleid, userid=userid, comments=comments, timestamp=timestamp, weight=weight, length=length, lat=lat, lon=lon)
-
-    db.session.add(new_tagEvent)
-    db.session.commit()
-
-    return new_tagEvent
+    
+    user = get_user (userid)
+    turtle = get_turtle (turtleid)
+    
+    if user and turtle:
+        new_tagevent = TagEvent (turtleid=turtleid, userid=userid, comments=comments, timestamp=timestamp, weight=weight, length=length, lat=lat, lon=lon)
+        
+        db.session.add(new_tagevent)
+        db.session.commit()
+        
+        return new_tagevent
+    
+    return None
