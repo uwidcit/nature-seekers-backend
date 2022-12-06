5e759c51e41313c1e6d3d99197dd45b86545f328
afeef10 <118063717+afeef10@users.noreply.github.com>
2022-12-05 20:33:08 -0400

    Update tagevent.py

--- App/controllers/tagevent.py
+++ App/controllers/tagevent.py
@@ -1,22 +1,27 @@
-from App.models import Turtle, User, TagEvent
+from App.models import Turtle, TagEvent, Contributor
 from App.database import db
 
 
-def get_turtle (id):
-    return Turtle.query.get(id)
+#def get_turtle (id):
+#   return Turtle.query.get(id)
+
+#def get_user (id):
+#    return User.query.get(id)
+
+def get_tag_event (tageventid):
+    return TagEvent.query.get(tageventid)
 
 
-def get_user (id):
-    return User.query.get(id)
-
-
-def create_tag_event (turtleid, userid, comments, timestamp, weight, length, lat, lon):
+def create_tag_event (turtleid, contributorid, comments, weight, length, lat, lon):
     
-    user = get_user (userid)
-    turtle = get_turtle (turtleid)
+    #user = get_user (userid)
+    #turtle = get_turtle (turtleid)
+
+    contributor = Contributor.get_contributor(id)
+    turtle = Turtle.get_turtle(id)
     
-    if user and turtle:
-        new_tagevent = TagEvent (turtleid=turtleid, userid=userid, comments=comments, timestamp=timestamp, weight=weight, length=length, lat=lat, lon=lon)
+    if contributor and turtle:
+        new_tagevent = TagEvent (turtleid=turtleid, contributorid=contributorid, comments=comments, weight=weight, length=length, lat=lat, lon=lon)
         
         db.session.add(new_tagevent)
         db.session.commit()
@@ -24,3 +29,12 @@ def create_tag_event (turtleid, userid, comments, timestamp, weight, length, lat
         return new_tagevent
     
     return None
+
+
+def get_all_tag_events_json():
+    tag_events = TagEvent.query.all()
+
+    if not tag_events:
+        return []
+
+    return [tagevent.toJSON() for tagevent in tag_events]
