dee0ffa15be86332728f9934138df44b8efc5a3c
kedwards <kwasi.edwards@sundaebytes.com>
2023-01-26 21:27:30 +0000

    removed invalid view function

--- App/views/media.py
+++ App/views/media.py
@@ -13,7 +13,8 @@ media_views = Blueprint('media_views', __name__, template_folder='../templates')
 @media_views.route('/api/media', methods=['GET'])
 def get_media_action():
     # all_media = get_all_media_json()
-    return jsonify(media)
+    # return jsonify(media)
+    pass
 
 @media_views.route('/api/media', methods=['POST'])
 def create_media_action():
